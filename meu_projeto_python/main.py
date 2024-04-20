691988e4c3c33e7dce4671554565e259b6daca26
Victoria Trois <vickytrois@outlook.com>
2024-04-20 16:18:22 -0300

    Cria um projeto com Python puro para experimentar comandos git.

---
+++ meu_projeto_python/main.py
@@ -0,0 +1,15 @@
+# Bem-vinde ao clone do repositório Oficina de Git e GitHub da Jornada Acadêmica da Informática!
+
+# Parabéns, você clonou um repositório, agora vamos modificar arquivos e embalá-los para levar.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
